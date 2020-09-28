23cd335688e022201b26c0117d843deb8b46aa8f
bulklc <clog@ucdavis.edu>
2020-09-27 21:22:52 -0700

    Changes to be committed: 	modified:   hw03/hw03_part_002.html 	modified:   hw03/hw03_part_003.html 	modified:   scripts/misc_functions.js

--- scripts/misc_functions.js
+++ scripts/misc_functions.js
@@ -17,3 +17,19 @@ function frmt_accntg_usd(num, ssize)
     return "$" + Array(fill_space).join(" ") + number_str
   }
 
+function isElementInViewport (el) {
+
+    // Special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+    );
+}
